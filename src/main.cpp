9a7a6f528a2cb7ab867c3cb14fff962edfb644ec
william huang <w02.huang@surreyschools.ca>
2025-05-25 15:54:16 -0700

    finalized

--- src/main.cpp
+++ src/main.cpp
@@ -1,170 +1,153 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <WebServer.h>
+#include <WebSocketsServer.h>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
-#include <WiFi.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <WebSocketsServer.h>
 
+// ==== OLED Setup ====
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define OLED_RESET -1
+#define OLED_RESET    -1
 #define SCREEN_ADDRESS 0x3C
-bool isPressed = false;
-String keyState = "None";
-String espMode = "SoftAP";
-bool prevPressed = false;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// ==== WiFi and Server Setup ====
+const char* ssid = "Pigeon";
+const char* password = "william is cool";
+WebServer server(80);
+WebSocketsServer webSocket(81);
 
+// ==== State Variables ====
+bool isPressed = false;
+bool prevPressed = false;
+String keyState = "None";
+String espMode = "SoftAP";
 
-void screenPrint(int x, int y, String text,bool debug = false){
-  display.setCursor(x,y);
+// ==== Utility Function ====
+void screenPrint(int x, int y, const String& text, bool debug = false) {
+  display.setCursor(x, y);
   display.println(text);
   display.display();
-  if (debug){
-    Serial.print("\n ");
+  if (debug) {
     Serial.println(text);
   }
-
 }
 
-
-
-//==============//
-//Website stuff//
-//==============//
-const char* ssid = "Pigeon";
-WebSocketsServer webSocket = WebSocketsServer(81);
-WebServer server(80); // Port 80 http server
-
-void handleRoot() { // At webpage http://192.168.4.1/
-
+// ==== Handle root index.html ====
+void handleRoot() {
   File file = SPIFFS.open("/index.html", "r");
-  if (!file){
-    Serial.println("File reading failed");
+  if (!file) {
+    Serial.println("Failed to open index.html");
+    server.send(500, "text/plain", "File Not Found");
+    return;
   }
   server.streamFile(file, "text/html");
   file.close();
-
 }
 
+// ==== Handle POST to /submit ====
 void handleSubmit() {
-  String json = server.arg("plain");  // Raw body of POST request
-  //Serial.println("Received JSON: " + json);
-
+  String json = server.arg("plain");
   DynamicJsonDocument doc(1024);
   DeserializationError error = deserializeJson(doc, json);
 
-  IPAddress senderIP = server.client().remoteIP();
-
   if (error) {
-    Serial.print("deserializeJson() failed: ");
+    Serial.print("JSON parse failed: ");
     Serial.println(error.c_str());
+    server.send(400, "text/plain", "Invalid JSON");
     return;
   }
 
-  String value = doc["data"];
-  Serial.println(senderIP.toString() + " " + value);
-  server.send(200, "text/plain", "pigeon recieved");
+  String message = doc["data"];
+  IPAddress senderIP = server.client().remoteIP();
+  Serial.println("[" + senderIP.toString() + "]: " + message);
 
+  webSocket.broadcastTXT("[" + senderIP.toString() + "]: " + message);
+  server.send(200, "text/plain", "Pigeon received");
 }
 
+// ==== WebSocket Event Handling ====
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch (type) {
+    case WStype_CONNECTED: {
+      IPAddress ip = webSocket.remoteIP(num);
+      Serial.printf("Client %u connected from %s\n", num, ip.toString().c_str());
+      break;
+    }
+    case WStype_DISCONNECTED:
 
+      Serial.printf("Client %u disconnected\n", num);
+      break;
+    case WStype_TEXT:
+      Serial.printf("Received message from %u: %s\n", num, payload);
+      break;
+    default:
+      break;
+  }
+}
+
+// ==== Setup Function ====
 void setup() {
   Serial.begin(115200);
-  Serial.println("starting");
+  Serial.println("Starting...");
 
-  WiFi.mode(WIFI_AP);
-  if (!WiFi.softAP(ssid)){
-    Serial.println("Soft AP creation failed");
-    return;
-  }
+  // OLED Init
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
 
+  // Button pin
+  pinMode(34, INPUT);
+
+  // SPIFFS Init
   if (!SPIFFS.begin(true)) {
     Serial.println("SPIFFS Mount Failed");
     return;
   }
-  
+
+  // WiFi Init
+  WiFi.mode(WIFI_AP);
+  if (!WiFi.softAP(ssid,password)) {
+    Serial.println("SoftAP Failed");
+    return;
+  }
+  Serial.println("SoftAP IP: " + WiFi.softAPIP().toString());
+
+  // HTTP Server Routes
   server.on("/", handleRoot);
+  server.on("/submit", HTTP_POST, handleSubmit);
   server.begin();
 
-  server.on("/submit",HTTP_POST, handleSubmit);
-
-  Serial.println(WiFi.softAPIP());
-  
-  //OLED
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-  display.clearDisplay();
-  pinMode(34,INPUT);
-
+  // WebSocket Init
   webSocket.begin();
-  webSocket.onEvent([](uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-    if (type == WStype_CONNECTED) {
-        IPAddress ip = webSocket.remoteIP(num);
-        Serial.printf("Client %u connected from %s\n", num, ip.toString().c_str());
-    } else if (type == WStype_DISCONNECTED) {
-        Serial.printf("Client %u disconnected\n", num);
-    } else if (type == WStype_TEXT) {
-        Serial.printf("Received message from %u: %s\n", num, payload);
-    }
-});
-
+  webSocket.onEvent(onWebSocketEvent);
 }
 
+// ==== Main Loop ====
 void loop() {
   server.handleClient();
   webSocket.loop();
 
+  // Button State Handling
+  isPressed = digitalRead(34);
+  if (prevPressed != isPressed) {
+    keyState = isPressed ? "kDown" : "kUp";
+    prevPressed = isPressed;
 
-  //OLED
-  //Serial.println(digitalRead(34));
-   if (digitalRead(34)){
-    isPressed = true;
-  }
-  else{
-    isPressed = false;
-  }
-  if (prevPressed == isPressed){
-      keyState = "None";
-    }
-    else if (isPressed == false){
-      keyState = "kUp";
-    } 
-    else if(isPressed == true){
-      keyState = "kDown";
-    }
-  prevPressed = isPressed;
-  
-  
-  if (keyState == "kDown"){
-    Serial.println("You pressed a button!");
-    if (espMode == "SoftAP"){
-      espMode = "HardAP";
+    if (keyState == "kDown") {
+      espMode = (espMode == "SoftAP") ? "HardAP" : "SoftAP";
       display.clearDisplay();
-    }
-    else{
-      espMode = "SoftAP";
-      display.clearDisplay();
-      Serial.println("global broadcasted");
-      webSocket.broadcastTXT("CALL:doSomething");
     }
   }
 
+  display.clearDisplay();
+  screenPrint(0, 0, "ESP32 State = " + espMode);
   display.setCursor(0, 20);
-  display.setTextColor(SSD1306_WHITE);
-  // Serial.print("cursor has been set\n");
-  // Serial.print("display cleared 1x\n");
-  
-  display.setTextSize(1);
-  // Serial.print("text size set\n");
   display.println("PIGEON");
-
   display.display();
-  // Serial.print("Drew pixels, display()'d as well.\n");
-  screenPrint(0,0,"ESP32 State = " + espMode);
-
-  
 }
